6a6669a90ea195bf90d9ae15e7cd9945a0add1ef
Adam Conder <adamdconder@gmail.com>
2016-04-19 13:36:03 +0100

    updating test dependencies

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -58,8 +58,8 @@ private object AppDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % "2.2.6" % scope,
-        "org.pegdown" % "pegdown" % "1.5.0" % scope,
-        "org.jsoup" % "jsoup" % "1.7.3" % scope,
+        "org.pegdown" % "pegdown" % "1.6.0" % scope,
+        "org.jsoup" % "jsoup" % "1.8.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
       )
     }.test
